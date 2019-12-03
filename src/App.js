a4fa1a654c4e3762c3d1746ea57b8d1211814eca
Jonathan <jonathanchuka@gmail.com>
2019-12-03 00:45:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import List from './List';
+import GroceryForm from './GroceryForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { groceries: [] }
+
+  getUniqId = () => {
+   //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+addItem = (name) => {
+const { groceries } = this.state;
+const grocery = { name, id: this.getUniqId() , complete: false }
+this.setState({ groceries: [grocery, ...groceries] }); 
+}
+
+handleClick = (id) => {
+  const { groceries } = this.state;
+  this.setState({
+    groceries: groceries.map( grocery => {
+      if (grocery.id === id) {
+        return {
+          ...grocery,
+          complete: !grocery.complete
+        }
+      }
+      return grocery
+    })
+  })
+}
+
+render() {
+const { groceries } = this.state;
+
+return (
+<div>
+<GroceryForm addItem={this.addItem} />
+<List name="Grocery List" items={groceries} />
+</div>
+);
+}
 }
 
 export default App;
